a5fc00d7392f9b75bac5cf43d1d4aae9562f7179
Kevin <kevinkzc15@gmail.com>
2023-05-31 14:50:16 +0800

    Stop your bullshit Shlock, last warning

--- Quiz 3/Quiz_3App.swift
+++ Quiz 3/Quiz_3App.swift
@@ -1,17 +1,1 @@
-//
-//  Quiz_3App.swift
-//  Quiz 3
-//
-//  Created by Kevin De Baboon on 31/5/23.
-//
 
-import SwiftUI
-
-@main
-struct Quiz_3App: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
-}
